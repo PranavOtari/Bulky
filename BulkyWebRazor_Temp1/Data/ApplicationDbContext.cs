00f65894afd5dc01000fdd524911c59cdc050835
Pranav <pranavotari06@gmail.com>
2024-06-01 18:35:36 +0530

    CreatedCategoryUI_RazorProject

---
+++ BulkyWebRazor_Temp1/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using BulkyWebRazor_Temp1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWebRazor_Temp1.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+                );
+        }
+    }
+}
--- BulkyWebRazor_Temp1/Program.cs
+++ BulkyWebRazor_Temp1/Program.cs
@@ -1,9 +1,15 @@
+using BulkyWebRazor_Temp1.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
