9d6f93954ed95eb501c656cbd5e073ebc30d43c2
Pranav <pranavotari06@gmail.com>
2024-06-08 23:36:27 +0530

    Added_ForeignKey_ForProductCategory_Relation

--- Bulky.Models/Product.cs
+++ Bulky.Models/Product.cs
@@ -5,6 +5,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Bulky.Models
 {
@@ -46,6 +47,10 @@ namespace Bulky.Models
         [Range(1, 1000)]
         public double Price100 { get; set; }
 
+        public int CategoryId { get; set; }
+        
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
 
 
 
